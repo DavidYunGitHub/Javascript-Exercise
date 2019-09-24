7800edda2daa317037cd46e3b80152fbdbcaeb58
David Yun <ff7yeah@yahoo.com>
2019-09-24 01:12:30 -0500

    test

--- JS-Exercise.js
+++ JS-Exercise.js
@@ -1,13 +1,19 @@
-var animals = ["goat", "cat", "crow"];
-var products = ["milk", "cheese", "burger"];
-var foodItems = [];
-var k = 0;
+// var animals = ["goat", "cat", "crow"];
+// var products = ["milk", "cheese", "burger"];
+// var foodItems = [];
+// var k = 0;
 
-for (var i = 0; i < animals.length; i++) {
-  for (var j = 0; j < products.length; j++) {
-    foodItems[k] = animals[i] + products[j];
-    k++;
-  }
-} 
+// for (var i = 0; i < animals.length; i++) {
+//   for (var j = 0; j < products.length; j++) {
+//     foodItems[k] = animals[i] + products[j];
+//     k++;
+//   }
+// } 
 
-console.log(foodItems);
+// console.log(foodItems);
+
+function askName() {
+  var userName = prompt("Enter name");
+}
+
+askName();
